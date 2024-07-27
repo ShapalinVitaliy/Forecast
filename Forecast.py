b82dbec80efbc7909ba428924763e9b6942adf51
ShapalinVitaliy <109358508+ShapalinVitaliy@users.noreply.github.com>
2024-07-27 15:44:02 +0300

    Разделение данных на test и train.
    Разделение данных на тренировочные 3 года и тестовый 4й год. Сохранение в соответствующие файлы

--- Forecast.py
+++ Forecast.py
@@ -2,37 +2,12 @@ import pandas as pd
 from datetime import datetime
 import numpy as np
 
-# Чтение CSV файла в DataFrame
-file_path = 'weatherHistory.csv'
-df = pd.read_csv(file_path, usecols=['Formatted Date', 'Temperature (C)', 'Wind Speed (km/h)', 'Pressure (millibars)'])
-df = df.transpose()
-data_array = df.values
+# Чтение из бинарного файла
+loaded_matrix = np.load('matrix.npy', allow_pickle=True)
 
-
-# Вывод первых 5 строк таблицы для проверки
-print(df.head())
-
-# Преобразование строк в объекты datetime
-date_objects = [datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f %z') for date in data_array[0]]
-
-data_array[0] = [datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f %z') for date in data_array[0]]
-# Сортировка объектов datetime
-date_objects_sorted = sorted(date_objects)
-
-data_array[1] = data_array[1][np.argsort(data_array[0])]
-data_array[2] = data_array[2][np.argsort(data_array[0])]
-data_array[3] = data_array[3][np.argsort(data_array[0])]
-
-data_array[0] = data_array[0][np.argsort(data_array[0])]
-# Преобразование обратно в строки
-data_array[0] = [date.strftime('%Y-%m-%d %H:%M:%S.%f %z') for date in date_objects_sorted]
+train_data = loaded_matrix[:4, 0:26304]
+test_data = loaded_matrix[:4, 26304:35040]
 
 # Сохранение в бинарный файл
-np.save('matrix.npy', data_array)
-
-# Чтение из бинарного файла
-#loaded_matrix = np.load('matrix.npy', allow_pickle=True)
-#print(loaded_matrix)
-
-# Вывод отсортированных дат
-print(data_array[0])
+np.save('train_data.npy', train_data)
+np.save('test_data.npy', test_data)

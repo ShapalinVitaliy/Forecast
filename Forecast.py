690ea68870037f5a303ee72241e4c10414764097
ShapalinVitaliy <109358508+ShapalinVitaliy@users.noreply.github.com>
2024-07-31 14:14:14 +0300

    Bidirectional GRU
    Двунаправленная GRU

--- Forecast.py
+++ Forecast.py
@@ -42,7 +42,7 @@ X_test, y_test = create_dataset(test_data)
 model = Sequential()
 
 model.add(Input(shape=(100, 1)))   # Входной слой
-model.add(Bidirectional(LSTM(35)))
+model.add(Bidirectional(GRU(40)))
 model.add(Dense(1))  # Один выходной нейрон
 
 # Компиляция модели
